9ce95fb2f8abe86598e1e1ae9c91a1654d9b3982
TOmI mARKO <tomekmarkowicz3@wp.pl>
2018-11-07 12:17:38 +0100

    Add project files.

---
+++ TAIO_MCGREGOR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAIO_MCGREGOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
